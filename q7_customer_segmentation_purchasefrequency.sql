2ad1ed3dcd87ee91b6f03e440b5c7f0edd89a9b8
charisseraso <charisseraso@gmail.com>
2026-09-20 18:37:41 +0800

    Update q7_customer_segmentation_purchasefrequency.sql

--- q7_customer_segmentation_purchasefrequency.sql
+++ q7_customer_segmentation_purchasefrequency.sql
@@ -1,17 +1,23 @@
 -- Q7 How many customers are repeat vs one-time buyers?
 WITH customer_orders AS (
-    SELECT 
-        customer_id,
-        COUNT(DISTINCT order_id) AS order_count
-    FROM orders
-    GROUP BY customer_id
+    SELECT
+        c.customer_unique_id,
+        COUNT(DISTINCT o.order_id) AS order_count
+    FROM customers c
+    JOIN orders o
+        ON c.customer_id = o.customer_id
+    GROUP BY c.customer_unique_id
 )
 
 SELECT
     CASE
-        WHEN order_count = 1 THEN 'One-time buyer'
-        ELSE 'Repeat buyer'
+        WHEN order_count = 1 THEN 'One-time Buyer'
+        ELSE 'Repeat Buyer'
     END AS customer_type,
-    COUNT(*) AS customer_count
+    COUNT(*) AS number_of_customers,
+    ROUND(
+        COUNT(*) * 100.0 / SUM(COUNT(*)) OVER (),
+        2
+    ) AS percentage
 FROM customer_orders
 GROUP BY customer_type;
